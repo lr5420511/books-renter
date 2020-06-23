1d53a52ff1c72ccffa57a6007a2748d78d50dfb6
lr5420511 <783621006@qq.com>
2020-06-23 13:08:31 +0800

    2

--- browser/src/app.vue
+++ browser/src/app.vue
@@ -10,8 +10,36 @@ export default {};
 
 <style lang="less">
 @import url(./assets/less/variables.less);
+@import url(./assets/less/mixins/device-mx.less);
+@import url(./assets/less/mixins/fill-mx.less);
+@import url(./assets/less/mixins/flex-mx.less);
+
+// 多屏幕分辨率适配
+.device-mx(@v-devices, @v-rem-percent);
+
+body {
+    .flex-mx();
+    margin: 0;
+    background-color: extract(@v-colors, 5);
+    font-family: @v-font-family;
+    font-size: @v-font-size;
+    color: extract(@v-colors, 3);
+    ul, li {
+        margin: 0;
+        padding: 0;
+    }
+    a {
+        .fill-mx();
+        color: extract(@v-colors, 3);
+    }
+}
 </style>
 
 <style lang="less" scoped>
 @import url(./assets/less/variables.less);
+@import url(./assets/less/mixins/flex-mx.less);
+
+div#root {
+    .flex-mx();
+}
 </style>
